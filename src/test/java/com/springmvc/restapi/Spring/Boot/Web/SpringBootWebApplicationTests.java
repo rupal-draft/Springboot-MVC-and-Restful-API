b0b2f0b15550ef5181f4c7c2c0e089ed884d6063
Rupal Paul <rupalpaulgspr@gmail.com>
2024-10-19 13:39:45 -0700

    Initial setup for spring mvc

---
+++ src/test/java/com/springmvc/restapi/Spring/Boot/Web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.restapi.Spring.Boot.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
