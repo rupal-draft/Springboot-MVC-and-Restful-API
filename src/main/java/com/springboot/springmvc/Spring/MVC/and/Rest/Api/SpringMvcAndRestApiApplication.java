a6cca23fef4449cd1ce03948f465888f567a7b05
Rupal Paul <rupalpaulgspr@gmail.com>
2024-10-21 06:01:25 -0700

    Dependencies

---
+++ src/main/java/com/springboot/springmvc/Spring/MVC/and/Rest/Api/SpringMvcAndRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springmvc.Spring.MVC.and.Rest.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAndRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAndRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springmvc/Spring/MVC/and/Rest/Api/SpringMvcAndRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springmvc.Spring.MVC.and.Rest.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAndRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
